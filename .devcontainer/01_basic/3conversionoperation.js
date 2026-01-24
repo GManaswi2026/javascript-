ddaa6314a472c49afd14cec685e4cd2fdf1189a9
GManaswi2026 <gmanaswi9@gmail.com>
2026-01-24 14:43:30 +0000

    conversionsoperation

--- .devcontainer/01_basic/3conversionoperation.js
+++ .devcontainer/01_basic/3conversionoperation.js
@@ -5,6 +5,7 @@ let score = true
 console.log(typeof score);
 console.log(typeof(score));
 
+
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
@@ -27,3 +28,38 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// operations************************
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+ 
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2); power mei
+//console.log(2%2);
+//console.log(2/2);
+
+
+let str1 ="hello"
+let str2="manaswi"
+let str3 = str1+str2+str1;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+ 2 +2); //string first then sko string k trh treat kiya jayega
+console.log(1+ 2 +"2");// string last mei then pehle k conversion ho jayega 
+
+console.log(+true);
+console.log(+"")
+ 
+let num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
+//study prefix and postfix through mdn
