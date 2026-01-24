31e977ad360c7dcef4ab176fdad336b799460a88
GManaswi2026 <gmanaswi9@gmail.com>
2026-01-24 15:06:02 +0000

    comparisons

---
+++ .devcontainer/01_basic/04comparison.js
@@ -0,0 +1,21 @@
+// console.log(2==1);  ctrl+/ comment at once
+// console.log(2<=1);
+// console.log(2>=1);
+
+// console.log( "2" > 1); change in data type toh problem h
+// console.log("02" >= 1);
+
+
+//avoid these types of conversion 
+console.log(null>0);       
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//avoid these types of conversion 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//===
+console.log("2" === 2);
